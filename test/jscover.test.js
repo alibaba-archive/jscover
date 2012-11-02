df5480e0f940ad0fd17920f9e38dc763ed9e38c7
fengmk2 <fengmk2@gmail.com>
2012-11-03 01:36:02 +0800

    finish first version

---
+++ test/jscover.test.js
@@ -0,0 +1,18 @@
+/*!
+ * jscover - test/jscover.test.js
+ * Copyright(c) 2012 fengmk2 <fengmk2@gmail.com>
+ * MIT Licensed
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var jscover = require('../');
+var should = require('should');
+
+describe('jscover.test.js', function () {
+
+});
